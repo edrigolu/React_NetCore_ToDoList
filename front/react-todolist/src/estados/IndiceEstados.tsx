e72804752d8c0c1f7dd404ce116385026fae5afa
edrigolu <edwinricardo.gonzalezluque@hotmail.com>
2022-07-25 15:41:51 -0500

    Implementacion borrar estado. Se utiliza SweetAlert2

--- front/react-todolist/src/estados/IndiceEstados.tsx
+++ front/react-todolist/src/estados/IndiceEstados.tsx
@@ -5,6 +5,7 @@ import { urlEstados } from "../utils/endpoints";
 import ListadoGenerico from "../utils/ListadoGenerico";
 import Paginacion from "../utils/Paginacion";
 import { estadoDTO } from "./estados.model";
+import confirmar from "../utils/Confirmar";
 
 export default function IndiceEstados() {
     const [estados, setEstados] = useState<estadoDTO[]>();
@@ -13,17 +14,33 @@ export default function IndiceEstados() {
     const [pagina, setPagina] = useState(1);
 
     useEffect(() => {
+        cargarDatos();
+    }, [pagina, recordsPorPagina])
+
+
+    function cargarDatos() {
         axios.get(urlEstados, {
             params: { pagina, recordsPorPagina }
         })
             .then((respuesta: AxiosResponse<estadoDTO[]>) => {
                 const totalDeRegistros = parseInt(respuesta.headers['cantidadtotalregistros'], 10);
                 setTotalPaginas(Math.ceil(totalDeRegistros / recordsPorPagina));
-
                 console.log(respuesta.data);
                 setEstados(respuesta.data);
             })
-    }, [pagina, recordsPorPagina])
+    }
+
+    async function borrar(id: number) {
+        try {
+            await axios.delete(`${urlEstados}/${id}`)
+            cargarDatos();
+        }
+        catch (error) {
+            console.log(error.response.data);
+        }
+    }
+
+
     return (
         <React.Fragment>
             <h3>Estados</h3>
@@ -46,7 +63,8 @@ export default function IndiceEstados() {
                             </td>
                             <td>
                                 <a className="btn btn-success" href={`/estados/editar/${estado.id}`}>Editar</a>
-                                <Button className="btn btn-danger">Borrar</Button>
+                                <Button onClick={() => confirmar(() => borrar(estado.id))}
+                                    className="btn btn-danger">Borrar</Button>
                             </td>
                         </tr>)}
                     </tbody>
@@ -55,10 +73,11 @@ export default function IndiceEstados() {
             <div className="form-group" style={{ width: '150px' }}>
                 <label>Registros por página:</label>
                 <select className="form-control"
-                defaultValue={10}
+                    defaultValue={10}
                     onChange={e => {
                         setPagina(1);
-                        setRecordsPorPagina(parseInt(e.currentTarget.value, 10))}}>
+                        setRecordsPorPagina(parseInt(e.currentTarget.value, 10))
+                    }}>
                     <option value={5}>5</option>
                     <option value={10}>10</option>
                     <option value={25}>25</option>
